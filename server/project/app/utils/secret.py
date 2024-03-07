1d9ac0e0cd2fed5be09af03dc5aee78f2eb60e0e
Madi-S <shaiken.m@mail.ru>
2024-03-07 16:17:12 +0500

    Async bcrypt used instead of blocking one

--- server/project/app/utils/secret.py
+++ server/project/app/utils/secret.py
@@ -1,27 +1,45 @@
 import uuid
-from passlib.context import CryptContext
+import asyncio
+import aiobcrypt
 
 
 class classproperty(property):
     def __get__(self, cls, owner):
+        """Wrapper for @property and @classmethod"""
         return classmethod(self.fget).__get__(None, owner)()
 
 
 class Secret:
-    pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-
     @classproperty
     def token(cls) -> str:
+        """Returns a random uuid4 token"""
         return 'token-' + str(uuid.uuid4())
 
     @classproperty
     def slug(cls) -> str:
+        """Returns a random 22-character length slug"""
         return uuid.uuid4().hex[:22]
 
     @classmethod
-    def get_password_hash(cls, password: str) -> str:
-        return cls.pwd_context.hash(password)
+    async def generate_password_hash(cls, password: bytes) -> bytes:
+        """Generates bytes password hash from given bytes password"""
+        salt = await aiobcrypt.gensalt()
+        hashed_password = await aiobcrypt.hashpw(password, salt)
+        return hashed_password
 
     @classmethod
-    def verify_password(cls, plain_password: str, hashed_password: str) -> bool:
-        return cls.pwd_context.verify(plain_password, hashed_password)
+    async def verify_password(cls, plain_password: bytes, hashed_password: bytes) -> bool:
+        """Validates whether or not plain password match with hash"""
+        return await aiobcrypt.checkpw(plain_password, hashed_password)
+
+
+async def _test():
+    """Test function for Secret password generation & verification"""
+    password = b'123qwerty'
+    hashed_password = await Secret.generate_password_hash(password)
+    verified = await Secret.verify_password(password, hashed_password)
+    return verified
+
+
+if __name__ == '__main__':
+    asyncio.run(_test())
